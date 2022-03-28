23c0976738b69b74c5502aab299e71f419e84cc1
amisha1502 <92580780+amisha1502@users.noreply.github.com>
2022-03-28 12:20:47 +0530

    Create insertdeletegetrandom.java

---
+++ insertdeletegetrandom.java
@@ -0,0 +1,34 @@
+public class RandomizedSet {
+
+    private List<Integer> list;
+    private Map<Integer,Integer> map;
+    java.util.Random rand = new java.util.Random();
+    public RandomizedSet() {
+        list=new ArrayList<>();
+        map=new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        map.put(val,list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        int index=map.get(val);
+        if(index!=list.size()-1){
+            int last=list.get(list.size()-1);
+            list.set(index,last);
+            map.put(last,index);
+        }
+        map.remove(val);
+        list.remove(list.size()-1);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
