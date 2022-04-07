c2bfd3ab9486d31ea557037e8e5c8c848c13961b
amisha1502 <92580780+amisha1502@users.noreply.github.com>
2022-04-07 09:38:38 +0530

    Create Pow(x, n).java

---
+++ Pow(x, n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0)
+        {
+            return 1;
+        }
+        if(n==Integer.MIN_VALUE)
+        {
+            x=x*x;
+            n=n/2;
+        }
+        if(n<0)
+        {
+            n=-n;
+            x=1/x;
+        }
+        double ans= myPow(x, n/2);
+        if(n%2==0) return ans*ans;
+        else return ans*ans*x;
+    }
+}
