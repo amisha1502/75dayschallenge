7970f9e152c85e7445b44800c1377f85683e29ae
amisha1502 <92580780+amisha1502@users.noreply.github.com>
2022-04-07 11:45:06 +0530

    Create Searcha2DMatrix.java

---
+++ Searcha2DMatrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row= matrix.length;
+        int col= matrix[0].length;
+        int i=0;
+        int j=matrix[0].length-1;
+        while(i>=0 && i<row && j>=0 && j<col)
+        {
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]<target) i++;
+        }
+        return false;
+    }
+}
